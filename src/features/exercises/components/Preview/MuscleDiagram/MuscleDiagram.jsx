86a6d4ecfcaaa956f5a685bfae2a8fc7731c830b
Misheelbat <p_misheel@me.com>
2022-07-25 17:19:52 +0200

    MuscleDiagram now only appears if there is data, changed preview component scrollbar style

--- src/features/exercises/components/Preview/MuscleDiagram/MuscleDiagram.jsx
+++ src/features/exercises/components/Preview/MuscleDiagram/MuscleDiagram.jsx
@@ -3,19 +3,23 @@ import { createImagesUrl } from 'features/exercises';
 import styles from './MuscleDiagram.module.css';
 
 export const MuscleDiagram = ({ primary = [], secondary = [] }) => {
-	const m = createImagesUrl(primary, secondary);
+	const { front, back } = createImagesUrl(primary, secondary);
 	const styleFront = {
-		backgroundImage: m.front.join(','),
+		backgroundImage: front.join(','),
 	};
 	const styleBack = {
-		backgroundImage: m.back.join(','),
+		backgroundImage: back.join(','),
 	};
 	return (
 		<div className={styles.muscleDiagram}>
 			<h4>MuscleDiagram</h4>
 			<div className={styles.diagramContainer}>
-				<div className={styles.diagram} style={styleFront} />
-				<div className={styles.diagram} style={styleBack} />
+				{front.length > 1 && (
+					<div className={styles.diagram} style={styleFront} />
+				)}
+				{back.length > 1 && (
+					<div className={styles.diagram} style={styleBack} />
+				)}
 			</div>
 		</div>
 	);
