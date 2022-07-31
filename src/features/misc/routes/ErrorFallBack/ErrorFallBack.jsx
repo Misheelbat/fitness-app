d0264c60f968be709a0453733baa659cca1507e9
Misheelbat <p_misheel@me.com>
2022-07-31 18:03:48 +0200

    errfallback page styled as well refresh button works and navigates to start page

--- src/features/misc/routes/ErrorFallBack/ErrorFallBack.jsx
+++ src/features/misc/routes/ErrorFallBack/ErrorFallBack.jsx
@@ -3,10 +3,13 @@ import { Button } from 'components/Elements';
 import styles from './ErrorFallBack.module.css';
 
 export const ErrorFallBack = () => {
+	const refreshPage = () => {
+		window.location.assign(window.location.origin);
+	};
 	return (
 		<div className={styles.errorFallBack}>
 			<h2>Oops, something went wrong :( </h2>
-			<Button>Refresh</Button>
+			<Button onClick={refreshPage}>Refresh</Button>
 		</div>
 	);
 };
