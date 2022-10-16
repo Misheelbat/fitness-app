502df9236b55c522a925b9b64a4a3a0ff482a4c4
Misheelbat <p_misheel@me.com>
2022-10-16 18:41:44 +0200

    removed unused code and npm packages

--- src/providers/app.jsx
+++ src/providers/app.jsx
@@ -2,13 +2,10 @@ import { Suspense } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 import { HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter } from 'react-router-dom';
-import { QueryClientProvider } from 'react-query';
-// import { ReactQueryDevtools } from 'react-query/devtools';
 import { Provider } from 'react-redux';
 
 import { reduxStore } from 'store';
 import { ToastProvider } from 'components/ToastContainer';
-import { queryClient } from 'lib';
 
 import { ErrorFallBack } from 'features/misc';
 import { PageSpinner } from 'components/Elements';
@@ -18,13 +15,10 @@ export const AppProvider = ({ children }) => {
 		<Suspense fallback={<PageSpinner />}>
 			<ErrorBoundary FallbackComponent={ErrorFallBack}>
 				<HelmetProvider>
-					<QueryClientProvider client={queryClient}>
 						<Provider store={reduxStore}>
 							<BrowserRouter>{children}</BrowserRouter>
 							<ToastProvider />
 						</Provider>
-						{/* <ReactQueryDevtools /> */}
-					</QueryClientProvider>
 				</HelmetProvider>
 			</ErrorBoundary>
 		</Suspense>
