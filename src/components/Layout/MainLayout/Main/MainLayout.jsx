a6598cf7d998030242eda0c7e316bf675f38b00a
Misheelbat <p_misheel@me.com>
2022-10-18 17:54:17 +0200

    fixed mainLayout not covering all content

--- src/components/Layout/MainLayout/Main/MainLayout.jsx
+++ src/components/Layout/MainLayout/Main/MainLayout.jsx
@@ -1,15 +1,21 @@
+import { useState } from 'react';
+
 import { Sidebar } from '../Sidebar/Sidebar';
 import Headerbar from '../Headerbar/Headerbar';
 
 import styles from './MainLayout.module.css';
-import { useState } from 'react';
+
 export const MainLayout = ({ children }) => {
 	const [mobileNavToggle, setMobileNavToggle] = useState(false);
+	
 	return (
 		<div className={styles.mainLayout}>
 			<Sidebar mobileNavToggle={mobileNavToggle} />
 			<section className={styles.content}>
-				<Headerbar mobileNavToggle={mobileNavToggle} setMobileNavToggle={setMobileNavToggle} />
+				<Headerbar
+					mobileNavToggle={mobileNavToggle}
+					setMobileNavToggle={setMobileNavToggle}
+				/>
 				<main>{children}</main>
 			</section>
 		</div>
